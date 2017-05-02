92b0f82190afce832eebd103f457ea59d0b60249
iapYang <yklorse@gmail.com>
2017-05-02 15:14:59 +0800

    update

--- bin/iapvt.js
+++ bin/iapvt.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const join = require('path').join;
+const shell = require('shelljs');
+const path = require('path');
 
 const defaultCommand = 'dev';
 
@@ -8,6 +9,7 @@ const commands = new Set([
     defaultCommand,
     'build',
     'bundle',
+    'gh',
 ]);
 
 let cmd = process.argv[2];
@@ -18,6 +20,24 @@ if (commands.has(cmd)) {
     cmd = defaultCommand;
 }
 
-const bin = join(__dirname, `iapvt-${cmd}`);
+let commandLine = '';
+let fileName = '';
 
-require(bin);
+switch (cmd) {
+    case 'dev':
+        fileName = path.join(__dirname, '../conf/webpack.dev.config.js');
+        commandLine = `webpack-dev-server --progress --colors --config ${fileName}`;
+        break;
+    case 'build':
+        fileName = path.join(__dirname, '../conf/webpack.dev.config.js');
+        commandLine = `webpack-dev-server --progress --colors --config ${fileName}`;
+        break;
+    default:
+        break;
+}
+
+shell.exec(commandLine, (code, stdout, stderr) => {
+    console.log('Exit code:', code);
+    console.log('Program output:', stdout);
+    console.log('Program stderr:', stderr);
+});
