f14ec1cbdc2fcaf077e95e0f1337e3487468eed0
iapYang <yklorse@gmail.com>
2017-05-05 17:36:24 +0800

    fail at no data when read

--- bin/iapvt-cli.js
+++ bin/iapvt-cli.js
@@ -7,6 +7,8 @@ ncp.limit = 16;
 const templatePath = path.join(__dirname, '../template');
 const destPath = path.join(process.cwd(), './tmp');
 const indexPath = path.join(destPath, './script/index.js');
+const routerPath = path.join(destPath, './router.js');
+const storePath = path.join(destPath, './store');
 
 const replaceContent = {
     imStore: `import store from '../store/index';`,
@@ -15,12 +17,45 @@ const replaceContent = {
     usRouter: `store,`,
 };
 
+function rmdir(dir) {
+    const list = fs.readdirSync(dir);
+    for (let i = 0; i < list.length; i++) {
+        const filename = path.join(dir, list[i]);
+        const stat = fs.statSync(filename);
+
+        if (filename === '.' || filename === '..') {
+            // pass these files
+        } else if (stat.isDirectory()) {
+            // rmdir recursively
+            rmdir(filename);
+        } else {
+            // rm fiilename
+            fs.unlinkSync(filename);
+        }
+    }
+    fs.rmdirSync(dir);
+}
+
 function replaceIndex(name) {
     // name is Store or Router
-    fs.accessSync(indexPath);
+    const fid = fs.openSync(indexPath, 'w+');
     const content = fs.readFileSync(indexPath, 'utf-8');
 
+    console.log(content);
+
     const replacement = content.replace(`// im${name}`, replaceContent[`im${name}`]).replace(`// us${name}`, replaceContent[`us${name}`]);
+
+    console.log(replacement);
+
+    fs.writeFileSync(indexPath, replacement, {
+        encoding: 'utf-8',
+    });
+
+    fs.closeSync(fid);
+}
+
+if (fs.existsSync(destPath)) {
+    return console.error('The diretory exits! Delete it first!');
 }
 
 ncp(templatePath, destPath, err => {
@@ -29,7 +64,19 @@ ncp(templatePath, destPath, err => {
     }
     console.log('template copy done!');
 
-    fs.unlinkSync(path.join(destPath, './router.js'));
+    const cmds = new Set(process.argv);
+
+    if (cmds.has('-r')) {
+        fs.unlinkSync(routerPath);
+    } else {
+        replaceIndex('Router');
+    }
+     
+    // if (cmds.has('-s')) {
+    //     fs.rmdir(storePath);
+    // } else {
+    //     replaceIndex('Store');
+    // }
 });
 
 
