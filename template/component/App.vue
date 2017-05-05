c2c83aee29b459fb3aac6bf97280a54c4d97e714
iapYang <yklorse@gmail.com>
2017-05-05 09:44:38 +0800

    add template

---
+++ template/component/App.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div class="app-container"></div>
+</template>
+
+<script>
+
+export default {
+    data() {},
+    components: {},
+};
+</script>
+
+<style lang="scss">
+
+</style>
