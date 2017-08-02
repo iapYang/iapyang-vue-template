6f459c83d9d3966b82a34afa44328f9c8f1f4b83
DESKTOP-8IQMI96\Creasy <yklorse@gmail.com>
2017-08-02 10:41:39 +0800

    still use ip on windows

--- conf/webpack.dev.config.js
+++ conf/webpack.dev.config.js
@@ -1,5 +1,7 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const os = require('os');
+const ip = require('ip');
 
 const baseWebpackConfig = require('./webpack.base.config.js');
 
@@ -7,7 +9,7 @@ module.exports = merge(baseWebpackConfig, {
     devtool: 'eval-source-map',
     devServer: {
         contentBase: './dev',
-        host: '0.0.0.0',
+        host: os.type() === 'Windows_NT' ? ip.address() : '0.0.0.0',
         open: true,
         hot: true,
         disableHostCheck: true,
