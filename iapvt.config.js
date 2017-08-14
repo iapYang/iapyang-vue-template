8a1a7395025bb91808cef6fbc4af39e01f1143ff
iapyang <yklorse@gmail.com>
2017-08-14 11:51:09 +0800

    i

--- iapvt.config.js
+++ iapvt.config.js
@@ -3,5 +3,8 @@ const path = require('path');
 module.exports = {
     cliPath: 'tmp',
     title: 'Index',
-    // template: path.resolve(__dirname, './template.html'),
+    bundle: {
+        path: path.resolve('./dev/component/App.vue'),
+        name: 'app',
+    },
 };
